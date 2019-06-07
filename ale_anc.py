0a08761f392478ee91b8e5baabb6a4394d65f751
Abhipray Sahoo <abhiprays@gmail.com>
2019-06-06 23:25:46 -0700

    refactoring

--- ale_anc.py
+++ ale_anc.py
@@ -6,42 +6,91 @@ import padasip as pa
 import numpy as np
 
 
-def ale_anc(x, fs, l=256, delta_ms=256, scheme=1):
-    """
-    Scheme 1 (only ale), Scheme 2 (ale+anc using only speech estimate), Scheme 3(ale+anc using speech+noise as primary)
-    :param fs: Sample rate of the audio file
-    :param x: input noisy speech signal 
-    :param l: length of the adaptive predictor
-    :param delta_ms: the number of samples into the future the linear predictor is predicting. 
-    Set it to higher thant correlation range for the noise signal but less than the correlation range for the speech signal. 
-    :param scheme: {1, 2 or 3} See description above.
-    :return: s_hat, n_hat, weights, d_ale
-    """
-    delta = int(delta_ms * fs / 1000)
+class AleDenoiser:
+    def __init__(self, fs, scheme, l1=256, l2=256, delta_ms=16):
+        self.fs = fs
+        self.scheme = scheme
+        self.l1 = l1
+        self.l2 = l2
+        self.W1 = None
+        self.W2 = None
+        self.delta = int(delta_ms * self.fs / 1000)
+        assert (self.scheme in {1, 2, 3})
 
-    x_ale = pa.input_from_history(np.concatenate((np.zeros(delta, ), x)), l)
+    def ale_anc(self, x):
+        """
+        Scheme 1 (only ale), Scheme 2 (ale+anc using only speech estimate), Scheme 3(ale+anc using speech+noise as primary)
+        :param fs: Sample rate of the audio file
+        :param x: input noisy speech signal
+        :param l: length of the adaptive predictor
+        :param delta_ms: the number of samples into the future the linear predictor is predicting.
+        Set it to higher thant correlation range for the noise signal but less than the correlation range for the speech signal.
+        :param scheme: {1, 2 or 3} See description above.
+        :return: s_hat, n_hat
+        """
+        x_ale = pa.input_from_history(
+            np.concatenate((np.zeros(self.delta, ), x)), self.l1)
 
-    # Create desired signal
-    d_ale = np.zeros((x_ale.shape[0], ))
-    for i in range(delta, x_ale.shape[0]):
-        d_ale[i - delta] = x_ale[i][0]
+        # Create desired signal
+        d_ale = np.zeros((x_ale.shape[0], ))
+        for i in range(self.delta, x_ale.shape[0]):
+            d_ale[i - self.delta] = x_ale[i][0]
 
-    # ale
-    f = pa.filters.FilterNLMS(n=l, mu=2, w="random")
-    s_hat, n_hat, weights = f.run(d_ale, x_ale)
+        # ale
+        f = pa.filters.FilterNLMS(n=self.l1, mu=2, w="random")
+        s_hat, n_hat, self.W1 = f.run(d_ale, x_ale)
 
-    # anc; delay primary input by L/2 samples to allow prediction filter to have two sided impulse response
-    if scheme == 2 or scheme == 3:
-        f2 = pa.filters.FilterNLMS(n=l, mu=0.1, w="random")
-        x_anc = pa.input_from_history(n_hat, l)
-        # Delay d_anc by N/2
-        if scheme == 2:
-            d_anc = s_hat[:x_anc.shape[0] - l // 2]
+        # anc; delay primary input by L/2 samples to allow prediction filter to have two sided impulse response
+        if self.scheme == 2 or self.scheme == 3:
+            f2 = pa.filters.FilterNLMS(n=self.l2, mu=0.1, w="random")
+            x_anc = pa.input_from_history(n_hat, self.l2)
+            # Delay d_anc by N/2
+            if self.scheme == 2:
+                d_anc = s_hat[:x_anc.shape[0] - self.l2 // 2]
+            else:
+                d_anc = x[:x_anc.shape[0] - self.l2 // 2]
+            d_anc = np.concatenate([np.zeros(self.l2 // 2), d_anc])
+            x_anc = x_anc[:len(d_anc), :]
+            n_hat, s_hat, self.W2 = f2.run(d_anc, x_anc)
+
+        return s_hat, n_hat
+
+    def feed_forward(self, x):
+        """Pass the signal x through the learned filter weights"""
+        if self.W1 is None or (self.scheme >= 2 and self.W2 is None):
+            return None
+
+        x_ale = pa.input_from_history(
+            np.concatenate((np.zeros(self.delta, ), x)), self.l1)
+
+        # Create desired signal
+        d_ale = np.zeros((x_ale.shape[0], ))
+        for i in range(self.delta, x_ale.shape[0]):
+            d_ale[i - self.delta] = x_ale[i][0]
+
+        # ale
+        e = []
+
+        y = []
+        for i in range(self.W1.shape[0]):
+            val = np.dot(self.W1[i], x_ale[i])
+            y.append(val)
+            e.append(d_ale[i] - val)
+
+        if self.scheme == 1:
+            return np.array(y)
+
+        # Feed the error signal into the second filter and set the desired filter as the delayed version of the
+        # output of the previous AF
+        x_anc = pa.input_from_history(e, self.l2)
+        if self.scheme == 2:
+            d_anc = y[:x_anc.shape[0] - self.l2 // 2]
         else:
-            d_anc = x[:x_anc.shape[0] - l // 2]
-        d_anc = np.concatenate([np.zeros(l // 2), d_anc])
-        x_anc = x_anc[:len(d_anc), :]
-        print(d_anc.shape, x_anc.shape)
-        n_hat, s_hat, weights = f2.run(d_anc, x_anc)
+            d_anc = x[:x_anc.shape[0] - self.l2 // 2]
+        d_anc = np.concatenate([np.zeros(self.l2 // 2), d_anc])
 
-    return s_hat, n_hat, weights, d_ale
+        e = []
+        for i in range(self.W2.shape[0]):
+            val = np.dot(self.W2[i], x_anc[i])
+            e.append(d_anc[i] - val)
+        return np.array(e)
---
+++ analysis.py
@@ -0,0 +1,59 @@
+"""analysis.py
+
+This script runs different speech denoising schemes on a noisy dataset and dumps clean audio and metrics
+
+Generate the noisy dataset using generate_dataset.py script.
+"""
+import pathlib
+import librosa
+import padasip as pa
+import numpy as np
+
+from tqdm import tqdm
+from ale_anc import AleDenoiser
+
+noisy_dataset = pathlib.Path("dataset/speech_plus_noise/")
+clean_dataset = pathlib.Path("dataset/speech/")
+conditions = list(noisy_dataset.iterdir())[2:3]
+print("There are {} conditions".format(len(conditions)))
+
+sr = 16000
+
+
+def rms_energy(x):
+    return 10 * np.log10((1e-12 + x.dot(x)) / len(x))
+
+
+for scheme in tqdm(range(1, 4)):
+    print(scheme)
+    for condition in tqdm(conditions):
+        print(condition.stem)
+        all = set(condition.glob("*.wav"))
+        noise_only = set(condition.glob("*_noise.wav"))
+        noisy = all - noise_only
+
+        for sample in noisy:
+            # De-noise the noisy signal
+            y, sr = librosa.load(sample, sr)
+            ale_denoiser = AleDenoiser(sr, scheme=scheme)
+            s_hat, n_hat = ale_denoiser.ale_anc(y)
+
+            # Open the noise only file
+            noise, sr = librosa.load(
+                str(sample.parent) + str(sample).split(".wav")[-1] + "/" +
+                sample.stem + "_noise.wav", sr)
+
+            # Open the signal only file
+            speech, sr = librosa.load(clean_dataset / sample.name, sr)
+
+            # Pass speech and noise through the filter and compute output SNR
+            n_hat2 = ale_denoiser.feed_forward(noise)
+            s_hat2 = ale_denoiser.feed_forward(speech)
+
+            # Write results and clean audio files out
+
+            # Ignore the first 10% of the audio file for calculating output SNR
+            samp_idx = int(0.1 * len(s_hat2))
+            output_snr = rms_energy(s_hat2[samp_idx:]) - rms_energy(
+                n_hat2[samp_idx:])
+            print(output_snr)
